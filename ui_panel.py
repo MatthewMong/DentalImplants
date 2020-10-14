1f5bbdf0de16ba4d82e37642af5d4b5e903b2c09
mattthew.mong1999@gmail.com <n0t4u12!>
2020-10-13 17:02:14 -0700

    add deviance

--- ui_panel.py
+++ ui_panel.py
@@ -1,5 +1,5 @@
 import bpy
-
+from math import pi
 
 class InfoPanel(bpy.types.Panel):
     """Creates a Panel in the Object properties window"""
@@ -8,19 +8,43 @@ class InfoPanel(bpy.types.Panel):
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
     bl_context = "object"
-
+    def eulerToDegree(euler):
+        euler=euler%(2*pi)
+        if euler >pi:
+            euler=euler-(2*pi)
+        return (euler/(2*pi))*360
+#        if euler >=pi:
+#            euler=(pi)-euler
+#        return ( (euler % (pi)) / (2 * pi) ) * 360
+    def average(array, axis):
+        sum = 0
+        for obj in array:
+            sum = sum + InfoPanel.eulerToDegree(obj.rotation_euler[axis])
+        sum=sum/len(array)
+        return sum
     def draw(self, context):
         layout = self.layout
         obj = context.object
         if "Dental Implant" in obj.name:
+            implants = [obj for obj in context.scene.objects if obj.name.startswith("Dental Implant")]
+            print(implants)
             row1 = layout.row()
             row1.label(text="Active object is: " + obj.name)
-            row2 = layout.row()
-            row2.label(text="Orientation is: " + str(obj.rotation_euler[0]))
-        elif "Mandible" in obj.name:
-            layout.label(text="Appearance")
-            row = layout.row(align=True)
-            row.operator("object.simple_operator")
+#            row2 = layout.row()
+#            row2.label(text="X orientation is: " + str(InfoPanel.eulerToDegree(obj.rotation_euler[0])))
+#            row3 = layout.row()
+#            row3.label(text="Y orientation is: " + str(InfoPanel.eulerToDegree(obj.rotation_euler[1])))
+#            row4 = layout.row()
+#            row4.label(text="Z orientation is: " + str(InfoPanel.eulerToDegree(obj.rotation_euler[2])))
+            row5 = layout.row()
+            row5.label(text="X deviance is: " + str(abs(InfoPanel.eulerToDegree(obj.rotation_euler[0])-InfoPanel.average(implants, 0))))
+            row6=layout.row()
+            row6.label(text="Y deviance is: " + str(abs(InfoPanel.eulerToDegree(obj.rotation_euler[1])-InfoPanel.average(implants, 1))))
+            row7=layout.row()
+            row7.label(text="Z deviance is: " + str(abs(InfoPanel.eulerToDegree(obj.rotation_euler[2])-InfoPanel.average(implants, 2))))
+        layout.label(text="Appearance")
+        row = layout.row(align=True)
+        row.operator("object.simple_operator")
             
             
 class SimpleOperator(bpy.types.Operator):
@@ -33,7 +57,7 @@ class SimpleOperator(bpy.types.Operator):
         return context.active_object is not None
 
     def execute(self, context):
-        obj=bpy.context.object
+        obj=bpy.data.objects["Mandible"]
         mat = obj.active_material
         if mat.name == "Opaque":
             obj.active_material=bpy.data.materials["Translucent"]
